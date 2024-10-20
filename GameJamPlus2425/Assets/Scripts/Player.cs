ee83b8bde9f3aa015daac380b0f337adc657087e
CrimsonCrown <arthurscouto@outlook.com>
2024-10-20 16:32:12 -0300

    player melhor

--- GameJamPlus2425/Assets/Scripts/Player.cs
+++ GameJamPlus2425/Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private InputHandler _input;
+    private bool hpLow;
     public  Dictionary<string, bool> inventory;
     public Interactable currentInteractableObject;
     public int maxHealth = 100;
